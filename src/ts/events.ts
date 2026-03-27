3d416ffaa640b047a5e70ea414aee0f9f9f69cf5
Róisín Grannell <r.grannell2@gmail.com>
2026-03-27 15:39:06 +0000

    ch ch ch chaaanges

--- src/ts/events.ts
+++ src/ts/events.ts
@@ -29,7 +29,6 @@ function bindCursorEvents(context: Context) {
     context.state.cursor.pos = [mousePos().x, mousePos().y]
   });
 
-
   onMouseRelease(() => {
     const { ship, limitsBar } = context.state;
 
@@ -42,8 +41,7 @@ function bindCursorEvents(context: Context) {
     }
 
     // we do, so jump around, jump around
-
-    ship.pos = [mousePos().x, mousePos().y];
+    ship.pos = pos(mousePos().x, mousePos().y);
   })
 }
 
